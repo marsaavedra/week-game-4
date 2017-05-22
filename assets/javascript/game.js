7bec14769b0200b5c8cd8e3245448fcb0d71ef93
maria saavedra <mariasaavedra@marias-MacBook-Air.local>
2017-05-21 23:44:23 -0400

    character move to the right

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,20 @@
         
     }
 
-    var aayla= new heroine(200, 25, 5, "ayyla");
-    var padme= new heroine(150, 20, 8, "padme");
-    var rey= new heroine(200, 25, 5, "rey");
-    var kylo= new heroine(200, 25, 5, "kylo");
+    var aayla = new heroine(200, 25, 5, "#aayla");
+    var padme = new heroine(150, 20, 8, "#padme");
+    var rey = new heroine(200, 25, 5, "#rey");
+    var kylo = new heroine(200, 25, 5, "#kylo");
     var myJedi;
+
+    var jedis = [aayla, padme, rey, kylo];
+    game();
 
     function game() {
         //this is how we pick the fighter with the on click event
         $(".pic").on("click", function() {
             if($(this).attr("id")==="rey"){
-                //crate obj with rey's stats
+                myJedi= rey;
             } else if ($(this).attr("id")==="aayla"){
                 myJedi = aayla;
             } else if ($(this).attr("id")==="padme") {
@@ -28,6 +31,13 @@
             } else {
                myJedi = kylo;
             }
+            for (var i = 0; i < jedis.length; i++) {
+                //move all jedis to the enemies section in HTML
+                $("#enemies").append($(jedis[i].name));
+            } 
+            //move the character that you picked back to the your character section in HTML
+            $("#yourCharacter").append($(myJedi.name));
+            
 
 
             
